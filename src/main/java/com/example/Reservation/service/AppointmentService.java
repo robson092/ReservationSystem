7b6623c4c9e13e65e91b1e9c140f40cd6d89f642
Robert Salicki <robsal12@gmail.com>
2024-01-03 11:29:31 +0100

    02-adding-missing-files

---
+++ src/main/java/com/example/Reservation/service/AppointmentService.java
@@ -0,0 +1,11 @@
+package com.example.Reservation.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AppointmentService {
+
+
+
+
+}
---
+++ src/main/java/com/example/Reservation/service/PatientService.java
@@ -0,0 +1,47 @@
+package com.example.Reservation.service;
+
+import com.example.Reservation.repository.PatientRepository;
+import com.example.Reservation.model.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PatientService {
+
+    private final PatientRepository repository;
+
+    @Autowired
+    public PatientService(PatientRepository repository) {
+        this.repository = repository;
+    }
+
+    public Optional<Patient> getPatient(Integer id) {
+        return repository.findById(id);
+    }
+
+    public List<Patient> getAllPatients() {
+        return repository.findAll();
+    }
+
+    public List<Patient> getAllPatientsWithPage(Pageable page) {
+        return repository.findAll(page).getContent();
+    }
+
+    public Patient save(Patient patient) {
+        return repository.save(patient);
+    }
+
+    public Optional<Patient> deletePatient(int id) {
+        return repository.deleteById(id);
+    }
+
+    public boolean isPatientExist(Integer id) {
+        return repository.existsById(id);
+    }
+
+
+}
