20592779eb6e77a2f53f17c4c5842eb608d0933c
Robert Salicki <robsal12@gmail.com>
2024-01-08 11:46:10 +0100

    Added-searching-appointments-by-done-field

--- src/main/java/com/example/reservation/dto/AppointmentDTO.java
+++ src/main/java/com/example/reservation/dto/AppointmentDTO.java
@@ -7,15 +7,16 @@ import java.time.LocalDateTime;
 
 @Getter
 public class AppointmentDTO {
-    private int id;
-    private LocalDateTime date;
-    private int patientID;
-    private String patientName;
-    private String patientSurname;
-    private int doctorId;
-    private String doctorName;
-    private String doctorSurname;
-    private String doctorSpecialization;
+    private final int id;
+    private final LocalDateTime date;
+    private final int patientID;
+    private final String patientName;
+    private final String patientSurname;
+    private final int doctorId;
+    private final String doctorName;
+    private final String doctorSurname;
+    private final String doctorSpecialization;
+    private final boolean done;
 
     public AppointmentDTO(Appointment source) {
         this.id = source.getId();
@@ -27,5 +28,6 @@ public class AppointmentDTO {
         this.doctorName = source.getDoctor().getName();
         this.doctorSurname = source.getDoctor().getSurname();
         this.doctorSpecialization = source.getDoctor().getSpecialization();
+        this.done = false;
     }
 }
--- src/main/java/com/example/reservation/repository/AppointmentRepository.java
+++ src/main/java/com/example/reservation/repository/AppointmentRepository.java
@@ -3,8 +3,10 @@ package com.example.reservation.repository;
 import com.example.reservation.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
     Optional<Appointment> deleteById(int id);
+    List<Appointment> findByDone(boolean done);
 }
